52a29a1d4b111e3e29e6fe915aa4a3b749ae7850
negu63 <negu6633@gmail.com>
2022-07-31 18:45:19 +0900

    Add friend count to FriendList

--- src/FriendList.tsx
+++ src/FriendList.tsx
@@ -11,13 +11,20 @@ export function FriendList({ minAge = 1, maxAge = 200 }) {
     return friends;
   }, [minAge, maxAge]);
 
+  const friendCount = useLiveQuery(() => db.friends.count());
+
   return (
-    <ul>
-      {friends?.map((friend) => (
-        <li key={friend.id}>
-          {friend.name}, {friend.age}
-        </li>
-      ))}
-    </ul>
+    <>
+      <p>
+        Your have <b>{friendCount}</b> friends in total.
+      </p>
+      <ul>
+        {friends?.map((friend) => (
+          <li key={friend.id}>
+            {friend.name}, {friend.age}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
