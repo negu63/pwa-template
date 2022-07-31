ed4735d0b7e53b4caf26ff967247dac7a11e5605
negu63 <negu6633@gmail.com>
2022-07-31 19:09:49 +0900

    Add NotificationButton to App page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { AddFriendForm } from "./AddFriendForm";
 import { FriendContainer } from "./FriendContainer";
+import { NotificationButton } from "./NotificationButton";
 
 function App() {
   return (
     <div className="App">
       <AddFriendForm />
       <FriendContainer />
+      <NotificationButton />
     </div>
   );
 }
